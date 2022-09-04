34305921b748cded9455e9bc7e641a4184c7c896
Danila Sokov <danila.sockov@yandex.ru>
2022-09-04 22:23:44 +0300

    first stage

--- src/cinema_room_manager/Main.java
+++ src/cinema_room_manager/Main.java
@@ -1,5 +1,15 @@
 package cinema_room_manager;
 
 public class Main {
-    
+    public static void main(String[] args) {
+        System.out.println("Cinema:");
+        System.out.println("  1 2 3 4 5 6 7 8");
+        for (int i = 1; i <= 7; i++) {
+            System.out.print(i + " ");
+            for (int j = 1; j <= 8; j++) {
+                System.out.print("S ");
+            }
+            System.out.println();
+        }
+    }
 }
