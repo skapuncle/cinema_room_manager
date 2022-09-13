67f0029bf61160176ea9e8445051e4ec1ded981b
Yohn Carley <yohncarl@ox-a1.msk.21-school.ru>
2022-09-13 19:46:49 +0300

    stage 3

--- src/cinema_room_manager/Main.java
+++ src/cinema_room_manager/Main.java
@@ -4,31 +4,75 @@ import java.util.Scanner;
 
 public class Main {
     final static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-        System.out.println("Cinema:");
-        System.out.println("  1 2 3 4 5 6 7 8");
-        for (int i = 1; i <= 7; i++) {
-            System.out.print(i + " ");
-            for (int j = 1; j <= 8; j++) {
-                System.out.print("S ");
-            }
-            System.out.println();
-        }
+        room();
+    }
+
+    public static void room() {
         System.out.println("Enter the number of rows:");
         int rowCount = scanner.nextInt();
         System.out.println("Enter the number of seats in each row:");
         int seatsCount = scanner.nextInt();
+        System.out.println("Cinema:");
 
         int allSeats = rowCount * seatsCount;
 
-        if (allSeats <= 60) {
-            System.out.println("Total income:");
-            System.out.println("$" + allSeats * 10);
-        } else {
-            int highCoast = rowCount / 2;
-            int lowCoast = rowCount - highCoast;
-            System.out.println("Total:");
-            System.out.println("$" + (highCoast * seatsCount * 10 + lowCoast * seatsCount * 8));
+        String[][] roomChairs = new String[rowCount + 1][seatsCount + 1];
+
+        for (int i = 0; i < roomChairs.length; i++) {
+            for (int j = 0; j < roomChairs[i].length; j++) {
+                if (i + j == 0) {
+                    roomChairs[i][j] = " ";
+                    System.out.print(roomChairs[i][j] + " ");
+                } else if (i == 0) {
+                    roomChairs[i][j] = String.valueOf(j);
+                    System.out.print(roomChairs[i][j] + " ");
+                } else if (j == 0) {
+                    roomChairs[i][j] = String.valueOf(i);
+                    System.out.print(roomChairs[i][j] + " ");
+                } else {
+                    roomChairs[i][j] = "S";
+                    System.out.print(roomChairs[i][j] + " ");
+                }
+            }
+            System.out.println(" ");
         }
+
+        System.out.println("Enter a row number:");
+        int row = scanner.nextInt();
+        System.out.println("Enter a seat number in that row:");
+        int seat = scanner.nextInt();
+
+        if (allSeats < 60 || row <= rowCount / 2) {
+            System.out.println("Ticket price: $10 ");
+        } else {
+            System.out.println("Ticket price: $8");
+        }
+
+        System.out.println("Cinema:");
+
+        for (int i = 0; i < roomChairs.length; i++) {
+            for (int j = 0; j < roomChairs[i].length; j++) {
+                if (i == row && j == seat) {
+                    roomChairs[i][j] = "B";
+                    System.out.print(roomChairs[i][j] + " ");
+                } else if (i + j == 0) {
+                    roomChairs[i][j] = " ";
+                    System.out.print(roomChairs[i][j] + " ");
+                } else if (i == 0) {
+                    roomChairs[i][j] = String.valueOf(j);
+                    System.out.print(roomChairs[i][j] + " ");
+                } else if (j == 0) {
+                    roomChairs[i][j] = String.valueOf(i);
+                    System.out.print(roomChairs[i][j] + " ");
+                } else {
+                    roomChairs[i][j] = "S";
+                    System.out.print(roomChairs[i][j] + " ");
+                }
+            }
+            System.out.println(" ");
+        }
+
     }
 }
